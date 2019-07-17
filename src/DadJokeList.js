ef87e9696bd9e5c7fa7154f7895c28bb726adca2
Katharine Cooney <katharinecooney@gmail.com>
2019-07-17 17:22:42 -0500

    Add basic style to DadJokeList sidebar

--- src/DadJokeList.js
+++ src/DadJokeList.js
@@ -23,17 +23,22 @@ class DadJokeList extends Component {
     this.setState({
       jokes: jokes
     })
-    
-    }
+  }
   
 
   render() {
     let allJokes = this.state.jokes.map(joke => <p>{joke}</p>)
     return (
       <div className="DadJokeList">
-        <h1>Dad Jokes!</h1>
+        <div className="DadJokeList-sidebar">
+          <h1><span>Dad </span>Jokes!</h1>
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="Crying laughter face" />
+          <button className="DadJokeList-getmore">New Jokes</button>
+
+        </div>
+        
         <div className="DadJokeList">
-        {allJokes}
+          {allJokes}
         </div>
       </div>
     )
