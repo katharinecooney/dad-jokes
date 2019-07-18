44bf1c8ad9a49f4ddba71808ebb134746b7b9750
Katharine Cooney <katharinecooney@gmail.com>
2019-07-18 13:45:56 -0500

    Update handleVote to save votes in localStorage after setState

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,6 +14,7 @@ class JokeList extends Component {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
     }
     this.handleVote = this.handleVote.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount(){
@@ -29,9 +30,7 @@ class JokeList extends Component {
     while(jokes.length < this.props.numJokesToGet){
       let response = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
       jokes.push({text: response.data.joke, votes: 0, id: response.data.id});
-      
     }
-    
     this.setState({
       jokes: jokes
     })
@@ -46,13 +45,15 @@ class JokeList extends Component {
       jokes: st.jokes.map( j =>
         j.id === id ? { ...j, votes: j.votes + change } : j
       )
-    }))
+    }),
+    // this anon function runs after the state is set
+    // it will save the votes for each joke in localStorage
+    () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)))
   }
-  
 
-
-
-
+  handleClick(){
+    this.getJokes();
+  }
 
   render() {
     let allJokes = this.state.jokes.map(joke => <Joke key={joke.id} votes={joke.votes} text={joke.text} handleVote={this.handleVote} id={joke.id} upChange={1} downChange={-1}/>)
@@ -61,10 +62,8 @@ class JokeList extends Component {
         <div className="JokeList-sidebar">
           <h1 className="JokeList-title"><span>Dad </span>Jokes!</h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="Crying laughter face" />
-          <button className="JokeList-getmore">New Jokes</button>
-
+          <button onClick={this.handleClick} className="JokeList-getmore">New Jokes</button>
         </div>
-        
         <div className="JokeList-jokes">
           {allJokes}
         </div>
